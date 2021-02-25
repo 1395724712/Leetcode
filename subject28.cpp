25b47f5ac6752c652b2a61fdf1be589465bed74e
wendell <1395724712@qq.com>
2021-02-25 09:44:57 +0800

    20210225

--- subject28.cpp
+++ subject28.cpp
@@ -9,7 +9,6 @@
 
 #include<string>
 #include<vector>
-#include<iostream>
 using namespace std;
 
 class Solution {
@@ -21,7 +20,7 @@ public:
         for(int i = 1;i<s.size();i++){
             //如果两个值不相等
             while(j>0&&s[j]!=s[i]){
-                j = next[j-1];
+                j = next[j];
             }
             //如果两个值相等
             if(s[j]==s[i]){
@@ -29,30 +28,5 @@ public:
             }
             next.push_back(j);
         }
-    }
-    int strStr(string haystack, string needle) {
-        if(needle.size()==0){
-            return 0;
-        }
-        vector<int> next;
-        getNext(next,needle);
-
-        //然后开始遍历数组
-        int j = 0;
-        for(int i = 0;i<haystack.size();i++){
-            //如果不相等
-            while(j>0&&needle[j]!=haystack[i]){
-                j = next[j-1];
-            }
-            //如果二者相等
-            if(needle[j]==haystack[i]){
-                j++;
-            }
-            //存在该字符串
-            if(j==needle.size()){
-                return i - j+1;
-            }
-        }
-        return -1;
     }
 };
