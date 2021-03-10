1032f056a5632bd2c8a1628ea834b40b7606401f
wendell <1395724712@qq.com>
2021-03-10 16:14:30 +0800

    20210310 先提交然后换到vs修改

---
+++ offer59_1.cpp
@@ -0,0 +1,38 @@
+/*
+* Copyright@wh
+* Author:wh
+* Date:20210310
+* Description:滑动窗口的最大值
+* Resolution:维持一个从栈底到栈顶的递减序列
+*/
+
+#include<vector>
+#include<stack>
+#include<list>
+using namespace std;
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> res;
+        list<int> sta;
+        // sta.push_back(nums[0]);
+
+        for(int i =0;i<k;i++){
+            while(!sta.empty()&&nums[i]>sta.back()) sta.pop_back();
+            sta.push_back(nums[i]);
+        }
+        res.push_back(sta.front());
+
+        //然后依次压栈
+        for(int i = k;i<nums.size();i++){
+            if(nums[i-1]==sta.front()) sta.pop_front();
+
+            while((!sta.empty())&&nums[i]>sta.back()) sta.pop_back();
+            sta.push_back(nums[i]);
+
+            res.push_back(sta.front());
+        }
+
+        return res;
+    }
+};
---
+++ offer60.cpp
@@ -0,0 +1,43 @@
+/*
+* Copyright@wh
+* Author:wh
+* Date:20210310
+* Description:某一点数出现的概论
+* Resolution:dp[n][j] = dp[n-1][j-i]       0<=i<=6
+* 空间优化的那个没看明白，先写个基本的吧
+*/
+
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<double> dicesProbability(int n) {
+        vector<vector<int>> dp;
+
+        //初始化n=1的情况
+        dp.push_back(vector<int>{6,0});
+        for(auto elem:dp[0]){
+            elem = 1;
+        }
+
+        //然后开始根据n，基于公式逐个计算
+        for(int k = 1;k<n;k++){
+            dp.push_back(vector<int>{});
+            for(int j = k;j<(k+1)*6;j++){
+                int sum = 0;//计数值
+                for(int i = 1;i<=5;i++){
+                    if(j-i<=0) break;
+                    sum+=dp[k-1][j-i];
+                }
+                dp[k].push_back(sum);
+            }
+        }
+
+        int all = pow(6,n);//一共的可能性
+        vector<double>res;
+        for(int i = n;i<=6*n;i++){
+            res.push_back(1.0*dp[n-1][i-1]/(1.0*all));
+        }
+        return res;
+    }
+};
